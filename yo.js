6eee8921ecb80b06b9830eeb32075a6379459edd
Bearcute <bearcutexdxd@yandex.ru>
2022-07-07 18:04:09 +0300

    added random string generator

--- yo.js
+++ yo.js
@@ -1,20 +1,2 @@
-const { User, Form } = require('./db/models');
-
-async function test() {
-  // const asd = await User.create({
-  //   email: 'ya@mail.com',
-  //   pass: '1234',
-  //   isAdmin: true,
-  //   createdAt: new Date(),
-  //   updatedAt: new Date(),
-  // });
-  // const asd = await Form.create({
-  //   creator_id: 1,
-  //   nameEmployee: 'Mikael',
-  //   nameMentor: 'Vasya',
-  //   link: 'http://localhost:3000/qwerty',
-  //   createdAt: new Date(),
-  //   updatedAt: new Date(),
-  // });
-}
-test();
+const randomstring = require('randomstring');
+console.log(randomstring.generate());
