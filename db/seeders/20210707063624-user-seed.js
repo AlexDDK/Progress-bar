6a908f1d322e451c196ed09a1b4cff3464c0d7d8
AlexDDK <dhrsmp@gmail.com>
2022-07-07 12:41:15 +0300

    uniq fields

--- db/seeders/20210707063624-user-seed.js
+++ db/seeders/20210707063624-user-seed.js
@@ -35,6 +35,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('User', null, {});
+    await queryInterface.bulkDelete('Users', null, {});
   },
 };
